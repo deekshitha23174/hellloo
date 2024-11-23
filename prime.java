da699eabbff5e4f7163e8cd2bf763f13086dab0a
deekshitha <22h51a05f8@cmrcet.ac.in>
2024-11-23 20:59:49 +0530

    second commit

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class prime {
     public static void main(String[] args) {
-        int limit = 100; // Change this value to check prime numbers up to a different limit
+        int limit = 50; // Change this value to check prime numbers up to a different limit
         System.out.println("Prime numbers up to " + limit + ":");
 
         for (int num = 2; num <= limit; num++) {
